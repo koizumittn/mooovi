eb610e91548022d3ea8eaf6f92c70a43fb8bdffb
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2016-12-14 19:44:56 +0900

    マイグレーションファイルを編集

--- db/migrate/20161214103717_create_products.rb
+++ db/migrate/20161214103717_create_products.rb
@@ -1,8 +1,13 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
-      t.timestamps null: false
+      t.string  :title
+      t.text    :image_url
+      t.string  :director
+      t.text    :detail
+      t.date    :open_date
+      t.string  :open_date_string
+      t.timestamps
     end
   end
 end
