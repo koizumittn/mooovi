643138ddddac9d66b8941775f480cf8cb0447b2c
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2016-12-15 20:23:16 +0900

    マイグレーションファイルを編集

--- db/migrate/20161215111517_create_reviews.rb
+++ db/migrate/20161215111517_create_reviews.rb
@@ -1,8 +1,11 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-
-      t.timestamps null: false
+      t.string    :nickname
+      t.integer   :rate
+      t.text      :review
+      t.integer   :product_id
+      t.timestamps
     end
   end
 end
