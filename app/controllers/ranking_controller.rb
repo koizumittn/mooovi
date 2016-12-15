336e018eadbb7ea6715f0f4ecf802baf7828ec65
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2016-12-16 01:35:41 +0900

    レビュー件数の多い順に表示されるようにランキングを修正

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -4,6 +4,6 @@ class RankingController < ApplicationController
   before_action :get_ranking
 
   def get_ranking
-    @ranking = Product.limit(5)
+    @ranking = Review.group(:product_id).order('count_product_id DESC').count(:product_id).keys.map{|product_id| Product.find(product_id)}
   end
 end
