bca65e137113fd7c6023ecb77e39aa769503fe87
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2016-12-16 20:46:17 +0900

    モデルクラスに画像ファイルの設定を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :image,
+    style: { mudium: "300x300#", thumb: "100x100#" }
+  validates_attachment_content_type :image,
+    content_type: [ "image/jpg", "image/png", "image/jpeg" ]
+
 end
